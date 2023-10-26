de9658890c51efbfa9bd17e62f23878dc2543508
marciomartins12 <mmarciom1martins635@gmail.com>
2023-10-26 13:32:06 -0300

    ..>

--- src/Store/rootStoreRedux.jsx
+++ src/Store/rootStoreRedux.jsx
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
 import usuarioReducer from "./Reducer/usuarioReducer";
 
+//o root vai ser responsavel pra mostrar o reducer
+//o que entrar nesse combinereducers são exxatamente os reducer com seus states que podem ser acessados
 const rootStoreRedux = combineReducers({usuarioReducer});
+
 export default rootStoreRedux;
--- src/Store/store.jsx
+++ src/Store/store.jsx
@@ -1,5 +1,7 @@
 import { createStore } from "redux";
 
-import rootStoreRedux from "./rootStoreRedux"
+import rootStoreRedux from "./rootStoreRedux";
+//store responsavel por armazenar tudo.
+//ostore que vai ser responsavel pelo root que é a raiz de onde vai sair todos os reducers
 const Store = createStore(rootStoreRedux);
 export default Store;
