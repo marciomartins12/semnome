1b13593a961b81467e152e752f2e4a6f3b84f08f
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-01 14:57:49 -0300

    ...

--- src/Redux/Store.jsx
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import { createStore } from "redux";
-import Products from "./Reducers/products";
-const rootreducers = combineReducers({Products})
-const Store = createStore(rootreducers)
--- src/Store/Store.jsx
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { createStore } from "redux";
-import Products from "./Reducers/ProductReducer";
-const RootReducers = combineReducers({Products});
-const Store = createStore(RootReducers);
-export default Store;
