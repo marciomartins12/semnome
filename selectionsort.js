ea0d204dc9e677b925d7d5f770b80e9637f12a64
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-12 10:50:13 -0300

    selection sort

--- selectionsort.js
+++ selectionsort.js
@@ -1,26 +1,27 @@
-const lista = [1,2,4,2,1,4,5,6];
-
-
-
-for(let cont = 0 ; cont < lista.length ; cont++){
-
-
-    let valormenor =  acharmenorValor( cont, lista);
-    console.log("final");
-}
-
-
-
-function acharmenorValor(posicao, lista){
-    
-    let menorValor = posicao;
-    for(let atual = posicao; atual < lista.length; atual++){
-        
-        if(lista[atual] < lista[menorValor]){
-            menorValor = lista[atual];
+const lista  = [1, 2, 4, 12, 1, 0, 35, 19, 3];
+organizaLista();
+function organizaLista(){
+    for(let atual = 0; atual < lista.length; atual++){
+        let menorFunc = menorNumero(lista, atual);
+        if(lista[atual] > lista[menorFunc]){
+            let aux = lista[atual];
+            lista[atual] = lista[menorFunc];
+            lista[menorFunc] = aux;
         }
-        console.log(`lista : ${lista[posicao]} menor valor : ${atual}`)
     }
-    return menorValor;
-
+    console.log(lista);
 }
+
+
+
+
+function menorNumero(array, indice){
+    let valorEntrada = indice;
+    for(atual = indice; atual < lista.length; atual++){
+        if(array[atual] < array[valorEntrada]){
+            valorEntrada = atual ; 
+        }
+    }
+    return valorEntrada;  
+};
+
