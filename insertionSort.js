68b38798f86c3a91ec5ba2be498439b1a9ba765b
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-13 12:08:38 -0300

    insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -18,8 +18,9 @@ const lista  = [
 ];
 
 for(let atual = 1; atual < lista.length; atual ++ ){
-    console.log("aiaias")
+
     for(let verifica = atual; verifica > 0 && lista[verifica].valor < lista[verifica - 1].valor; verifica--){
+       
         let aux = lista[verifica];
         lista[verifica] = lista[verifica -1];
         lista[verifica -1] = aux;
