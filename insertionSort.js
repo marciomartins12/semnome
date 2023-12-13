6d15c4b86a46ac786dc31f35f6ce83bd24988e6d
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-13 11:57:42 -0300

    Inciando INsertion Sort

---
+++ insertionSort.js
@@ -0,0 +1,18 @@
+const lista  = [
+    {
+        nome : "teste",
+        valor : 50
+    },
+    {
+        nome : "teste",
+        valor : 0
+    },
+    {
+        nome : "Márcio",
+        valor : -20
+    },
+    {
+        nome : "teste",
+        valor : 3
+    }
+];
