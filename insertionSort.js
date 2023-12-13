bc962375f0f9a80dd1506108c154260a14d9e071
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-13 12:07:30 -0300

    insertion Sort funcionando

--- insertionSort.js
+++ insertionSort.js
@@ -16,3 +16,14 @@ const lista  = [
         valor : 3
     }
 ];
+
+for(let atual = 1; atual < lista.length; atual ++ ){
+    console.log("aiaias")
+    for(let verifica = atual; verifica > 0 && lista[verifica].valor < lista[verifica - 1].valor; verifica--){
+        let aux = lista[verifica];
+        lista[verifica] = lista[verifica -1];
+        lista[verifica -1] = aux;
+
+    };
+};
+console.log(lista);
