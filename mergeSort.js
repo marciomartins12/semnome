a47b2ba50f5187f6a1dce1bf174e041694bceebb
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-18 08:54:15 -0300

    inicializando as listas.

---
+++ mergeSort.js
@@ -0,0 +1,25 @@
+const lista1 = [{
+    nome : "Dean",
+    valor: 40,
+},
+{
+    nome : "Jhon",
+    valor: 100,
+}
+,{
+    nome: "Sammy",
+    valor : 1,
+}];
+const lista2 = [{
+    nome : "Barney",
+    valor : 14,
+},
+{
+    nome : "Marshal",
+    valor : 3,
+},
+{
+    nome : "Lilian",
+    valor : 7,
+}
+]
