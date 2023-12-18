74ec0d28a08427a6473a505a50896487409922f7
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-18 09:12:48 -0300

    acho que consegui.

--- mergeSort.js
+++ mergeSort.js
@@ -1,25 +1,55 @@
 const lista1 = [{
     nome : "Dean",
-    valor: 40,
+    valor: 1,
 },
 {
     nome : "Jhon",
-    valor: 100,
+    valor: 50,
 }
 ,{
     nome: "Sammy",
-    valor : 1,
+    valor : 60,
 }];
-const lista2 = [{
+const lista2 = [
+    {
+        nome : "Barney",
+        valor : 1,
+    },
+    {
+        nome : "Barney",
+        valor : 2,
+    },
+    {
+        nome : "Barney",
+        valor : 10,
+    },
+    {
     nome : "Barney",
     valor : 14,
 },
 {
     nome : "Marshal",
-    valor : 3,
+    valor : 15,
 },
 {
     nome : "Lilian",
-    valor : 7,
+    valor : 70,
 }
-]
+];
+for(let contadorLista1 = 0, contadorLista2 = 0, listaNova = [], contVez ;contadorLista1 < lista1.length && contadorLista2 < lista2.length; contVez++){
+    if(lista1[contadorLista1].valor < lista2[contadorLista2].valor){
+        listaNova.push(lista1[contadorLista1]);
+        contadorLista1++;
+    }
+    else if(lista1[contadorLista1].valor > lista2[contadorLista2].valor){
+        listaNova.push(lista2[contadorLista2]);
+        contadorLista2++;
+    }else if(lista1[contadorLista1].valor == lista2[contadorLista2].valor){
+        listaNova.push(lista1[contadorLista1]);
+        listaNova.push(lista2[contadorLista2]);
+        contadorLista1++;
+        contadorLista2++;
+    }
+    console.log("aaaaaaaaaaaaaaaaaaaaa")
+    console.log(listaNova)
+}
