1b0fe6cee1c46d752aed36f04796e9c9ca84eb22
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-22 11:23:43 -0300

    selection sort finalizado.

---
+++ selectionSort.js
@@ -0,0 +1,25 @@
+const lista = require("./listaDesordenada");
+const newArray = lista;
+
+for(let x = 0; x < newArray.length; x++){
+
+let valueMenor = menor(newArray, x);
+
+if(newArray[x].valor > newArray[valueMenor].valor){
+    let aux = newArray[x];
+    newArray[x] = newArray[valueMenor];
+    newArray[valueMenor] = aux;
+    };
+};
+
+function menor(array, indice){
+    let menorValor = indice;
+    
+    for(let posicao = indice + 1; posicao < array.length; posicao++){
+        if(array[menorValor].valor > array[posicao].valor){
+            menorValor = posicao;
+        };
+    };
+    return menorValor;
+};
+console.log(newArray);
