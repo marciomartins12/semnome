76b43f945c6fb8741a0381d85374c5495664e23d
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-29 19:13:50 -0300

    testando a sintaxe

---
+++ php/sintaxe.php
@@ -0,0 +1,18 @@
+<?php
+
+$minhaIdade = 10;
+$entrou = false;
+$acompanho = true;
+
+echo "\t-----------Festa--------\n";
+echo "So se pode entrar na festa maiores de 18 anos ou acomphado.\n";
+
+
+if($minhaIdade >= 18 || $acompanho){
+    $entrou = true;
+    
+}
+$reposta = $entrou? "\tpode entrar\n":"\tvoce nao pode entrar\n";
+
+
+echo $reposta;
