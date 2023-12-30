7b9789699655f88f0b4d438b4311eeab8f78fe0c
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-30 14:00:23 -0300

    aprendendo usar o count, ele funciona como a propriedade length do javascript, conta o tamanho do array, o comprimento dele

--- php/sintaxe.php
+++ php/sintaxe.php
@@ -1,17 +1,30 @@
 <?php
 
-$minhaIdade = 10;
-$entrou = false;
-$acompanho = true;
 
-echo "\t-----------Festa--------\n";
-echo "So se pode entrar na festa maiores de 18 anos ou acomphado.\n";
 
-if($minhaIdade >= 18 || $acompanho){
-    $entrou = true;
-    
+$lista = [44,55,48,7,5,"testesevai"];
+for($x = 0; $x< count($lista); $x++){
+    echo "exide indice: $x = posicao: ". $lista[$x]."\n";
 }
-$reposta = $entrou? "\tpode entrar\n":"\tvoce nao pode entrar\n";
+echo count($lista);
 
 
-echo $reposta;
+
+
+
+
+// $minhaIdade = 10;
+// $entrou = false;
+// $acompanho = true;
+
+// echo "\t-----------Festa--------\n";
+// echo "So se pode entrar na festa maiores de 18 anos ou acomphado.\n";
+
+// if($minhaIdade >= 18 || $acompanho){
+//     $entrou = true;
+    
+// }
+// $reposta = $entrou? "\tpode entrar\n":"\tvoce nao pode entrar\n";
+
+
+// echo $reposta;
