22a1ec7917b4bb7cb3a912eef5eebb0bed2b529d
marciomartins12 <mmarciom1martins635@gmail.com>
2023-12-31 12:34:23 -0300

    entendndo array de obj

--- php/sintaxe.php
+++ php/sintaxe.php
@@ -2,11 +2,26 @@
 
 
 
-$lista = [44,55,48,7,5,"testesevai"];
-for($x = 0; $x< count($lista); $x++){
-    echo "exide indice: $x = posicao: ". $lista[$x]."\n";
-}
-echo count($lista);
+
+
+$listaPessoas = [
+    'nome' => "Márcio José Martins Câmara",
+    'salario' => 123423.92,
+    'idade' => 21
+];
+
+echo "Info: nome:".$listaPessoas['nome']." idade:".$listaPessoas['idade']." salario:".$listaPessoas['salario'].".";
+
+
+
+
+
+
+// $lista = [44,55,48,7,5,"testesevai"];
+// for($x = 0; $x< count($lista); $x++){
+//     echo "exide indice: $x = posicao: ". $lista[$x]."\n";
+// }
+// echo count($lista);
 
 
 
