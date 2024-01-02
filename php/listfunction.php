74a119ec445ef4c55b34b78a064f3f2264ccfc86
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-02 14:20:44 -0300

    destruct

---
+++ php/listfunction.php
@@ -0,0 +1,12 @@
+<?php
+
+$usercurrent = [[
+        "testNome",
+        "testEmail",
+        "testeCpf",
+        "testTell",
+]    
+];
+
+    list($nome, $email, $cpf, $tel) = $usercurrent[0];
+    echo "nome = $nome\n email = $email\n cpf = $cpf\n telefone = $tel\n";
