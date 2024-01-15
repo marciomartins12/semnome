bb121b527e2e6be80e4e15d3edb71f71235a39a4
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-15 11:28:31 -0300

    sbstr

--- php/str.php
+++ php/str.php
@@ -2,5 +2,9 @@
 $string = "marcio jose martins camara 123456789";
 
 var_dump(str_contains($string, "jose"));
+
 var_dump(str_ends_with($string, "89"));
+
 var_dump(str_starts_with($string, "jose"));
+
+var_dump(substr($string, 9, 9));
