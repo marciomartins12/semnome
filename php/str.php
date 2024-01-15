913e857d39e9de423f3f49b6e10ef71032bd7f04
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-15 11:36:52 -0300

    .

--- php/str.php
+++ php/str.php
@@ -1,5 +1,14 @@
 <?php
-$string = "marcio jose martins camara 123456789";
+$string = "marcio jose martins camara @ 123456789";
+$pos = "";
+for($x = 0; $x < strlen($string); $x++){
+    if($string[$x] === "@"){
+        $pos = $x;
+    };
+};
+
+echo $x;
+
 
 var_dump(str_contains($string, "jose"));
 
@@ -8,3 +17,5 @@ var_dump(str_ends_with($string, "89"));
 var_dump(str_starts_with($string, "jose"));
 
 var_dump(substr($string, 9, 9));
+
+var_dump(substr($string,0, $pos));
