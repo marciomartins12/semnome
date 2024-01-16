b8e096b11cd1900f676ada95f3dc75e9a6de698e
marciomartins12 <mmarciom1martins635@gmail.com>
2024-01-16 13:52:12 -0300

    str_replace - substitui algo por algo.

--- php/implode.php
+++ php/implode.php
@@ -10,3 +10,12 @@ $array =  [
 $str = implode(" ", $array);
 
 var_dump($str);
+
+
+$string = "123.154.456-12";
+$cpf = explode(".", $string);
+$cpf = implode($cpf);
+$cpf = explode("-", $cpf);
+$cpf = implode($cpf);
+
+var_dump($cpf);
