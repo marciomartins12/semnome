ec9bd2cac14a06878b5d5c310159d86b79b6efb7
marciomartins12 <mmarciom1martins635@gmail.com>
2024-02-04 13:01:40 -0300

    composer

---
+++ php/Curso3/src/searchCurso.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+$client = new Client();
+$resposta = $client->request("GET", 'https://cursos.alura.com.br/user/marciom1martins635/fullCertificate/ec4a9e4212cf8ec94ce64ff1cee49545', [
+    'auth'=> ['user', 'pass']
+]);
+
+$html = $resposta->getBody();
+
+$crawler = new  Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter("li.content-courseList-courseName");
+
+foreach( $cursos as $curso){
+  echo $curso->textContent;
+};
