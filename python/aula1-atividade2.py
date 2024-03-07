982a0cc9556fe804f2f8a51c6958481f648f3b4d
marciomartins12 <mmarciom1martins635@gmail.com>
2024-03-07 11:01:40 -0300

    ..

--- python/aula1-atividade2.py
+++ python/aula1-atividade2.py
@@ -21,3 +21,12 @@ except:
 #     print("a chave existe")
 # else:
 #     print("chave não existe")
+        
+
+frase = "marcio marcio jose martins"
+contagem_palavras = {}
+palavras = frase.split()
+for palavra in palavras:
+    contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+    print(contagem_palavras)
+print(contagem_palavras)
