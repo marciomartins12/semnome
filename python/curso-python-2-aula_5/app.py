723434cc4562dad36423d882cce37b6e69354ece
marciomartins12 <mmarciom1martins635@gmail.com>
2024-03-14 12:41:59 -0300

    >

--- python/curso-python-2-aula_5/app.py
+++ python/curso-python-2-aula_5/app.py
@@ -1,13 +1,13 @@
 from modelos.restaurante import Restaurante
 from modelos.cardapio import ItemCardapio, Cardapio
+
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+
 
 def main():
     suco_de_Frutas = ItemCardapio("suco de amora", 2.50)
-
+    Restaurante.listar_restaurantes()
     suco_de_Frutas.exibeCardapio()
 if __name__ == '__main__':
     main()
