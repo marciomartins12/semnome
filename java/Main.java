2f4a192c138d489139b16d03a564e4a46fda4752
marciomartins12 <marciom1martins635@gmail.com>
2024-04-16 14:24:18 -0300

    Teste Scanner

--- java/Main.java
+++ java/Main.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner Scanf = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno");
+
+        String nameAluno = Scanf.nextLine();
+
+        System.out.println("Digite a idade do " + nameAluno);
+
+        int idadeAluno = Scanf.nextInt();
+
         Turma computacao = new Turma(7, "8:00");
 
-        Aluno donamorte = new Aluno("donamorte", 32, computacao);
+        Aluno donamorte = new Aluno(nameAluno, idadeAluno, computacao);
 
         donamorte.strngg();
         System.out.println(donamorte);
-        Aluno.getNumeroDeContas();
+        System.out.println(Aluno.getNumeroDeContas());
+
+        int a = 3, b = 9;
+        double x = (double) a / b;
+        System.out.println(x);
     }
 }
