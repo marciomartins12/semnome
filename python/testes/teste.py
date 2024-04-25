5d4510310c2a802a3411d69465eee1d80b29c2a1
marciomartins12 <marciom1martins635@gmail.com>
2024-04-25 15:58:08 -0300

    ..

--- python/testes/teste.py
+++ python/testes/teste.py
@@ -6,5 +6,21 @@ for i in range(10 - n):
     print("Numero", i)
 
 
-for index, i in enumerate(array, start=-12):
-    print(f"valor do indice {index}: {i}")    
+for a, i in enumerate(array, start=-12):
+    print(f"valor do indice {a}: {i}")    
+
+def modificaValue(valor):
+    valor = 25
+    print("valor na função", valor)
+
+valor =2
+modificaValue(valor)
+
+print("Valor fora da função ",valor)
+
+
+array2 = [1,3,1,2,3,4,4,5,55,23,32]
+
+array2.sort()
+arraySort = array2
+print(array2)
