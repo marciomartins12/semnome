77e69b88d081879d98febbfac1001aed2e794804
marciomartins12 <mmarciom1martins635@gmail.com>
2024-05-01 15:11:56 -0300

    .

---
+++ java123/Main.java
@@ -0,0 +1,8 @@
+package java123;
+public class Main{
+
+     public static void main(String[] args) {
+        System.out.println("aiaiiaiaa");
+        
+    }
+}
---
+++ python/tes/main.py
@@ -0,0 +1,8 @@
+kokiname = "koki Nam     e"
+
+kokoindex = int(len(kokiname))
+
+print("index : ",kokoindex)
+print("koki name : ", kokiname)
+
+print(kokiname[kokoindex - 1])
